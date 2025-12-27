96b79adc64a0f3df943d951a7d6237fb455dfa28
nrogick-creator <n.rogick@gmail.com>
2025-12-27 14:30:58 -0300

    first commit

---
+++ horadodia.js
@@ -0,0 +1,24 @@
+var agora = new Date()
+var hora = 10 //agora.getHours()
+console.log(`Agora são ${hora} horas.`)
+
+if (hora >= 6 && hora < 12){
+    console.log('Bom dia!')
+    <img, src="manha.png", alt="Imagem do dia">
+    document.body.style.backgroundcolor; '#e2cd9f'
+
+} else if (hora >= 12 && hora < 18){
+    console.log('Boa tarde!')
+    <img, src="tarde.png", alt="Imagem da tarde">
+    document.body.style.backgroundcolor; '#b9846f'
+
+} else if (hora >= 18 && hora < 24){
+    console.log('Boa noite!')
+    <img, src="noite.png", alt="Imagem da noite">
+    document.body.style.backgroundcolor; '#444458ff'
+
+} else {
+    console.log('Boa madrugada!')
+    <img, src="noite.png", alt="Imagem da madrugada">
+    document.body.style.backgroundcolor; '#1b1b24ff'
+}
